069e942e9827c85b90668f6b752b35dfa072626b
JhonMercedes <jhonzeras93@gmail.com>
2021-03-10 11:29:32 -0300

    Home concluido

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var windowHeight = window.innerHeight;
+var section = document.querySelectorAll('.ja-section')
+        
+    var i;
+    for(i=0; section.length>i; i++) {
+        var mySection = section[i];
+        mySection.style.height = (windowHeight-150)+'px';
+    }
